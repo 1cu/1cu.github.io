ccbd2c4b2c330fdb9c009fb092d82cbdf3b819db
1cu <me@1cu.de>
2019-02-24 09:48:08 +0100

    restructur folders

---
+++ pelican/content/article1.md
@@ -0,0 +1,10 @@
+Title: Der erste Post
+Date: 2019-02-23 23:00
+Modified: 2019-02-23 23:12
+Category: Blog
+Tags: pelican, publishing
+Slug: my-super-post
+Authors: Jens Bergmann
+Summary: Der erste Post im neuen Blog!
+
+Ein erster Testbeitrag für den neuen Blog - ob ich die alten Inhalte mit überführe?!
---
+++ pelican/pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Jens Bergmann'
+SITENAME = u'Logbuch @ 1cu.de'
+SITEURL = 'https://1cu.de'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = u'de'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/jnsbrgmnn'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
